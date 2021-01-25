6c5ab90a5c73eb89c968c47c8e693a2f4017969a
Lagoons Browning <lagoons@protonmail.com>
2021-01-25 11:18:55 -0600

    Added better naming conventions in inner module

--- lib/luhn/algo.ex
+++ lib/luhn/algo.ex
@@ -2,7 +2,7 @@ defmodule Luhn.Algo do
   def valid?(num) when is_integer(num) do
     list = Integer.digits(num)
     [first | rest] = Enum.reverse(list) 
-    luhn = Enum.map_every(rest, 2, &worker/1)
+    luhn = Enum.map_every(rest, 2, &pass_to_check/1)
     [first| luhn]
     |> Enum.sum()
     |> Integer.mod(10) == 0
@@ -12,8 +12,17 @@ defmodule Luhn.Algo do
     Enum.map(nums, &valid?/1)
   end
 
-  def worker(x) do
-    check(x*2)
+  def random_gen(x) do
+    derived = x - 1
+    new_len = :math.pow(10, derived)
+    num = Enum.random(round(new_len)..round(new_len * 9.9))
+    t = valid?(num)
+    validate_random(t, x, num)
+  end
+
+  def pass_to_check(x) do
+    x * 2
+    |> check()
   end
 
   defp check(x) when x > 9 do
@@ -24,19 +33,11 @@ defmodule Luhn.Algo do
 
   defp check(x) when x < 10, do: x
 
-  def random_gen(x) do
-    derived = x - 1
-    new_len = :math.pow(10, derived)
-    num = Enum.random(round(new_len)..round(new_len * 9.9))
-    t = valid?(num)
-    getter(t, x, num)
-  end
-
-  defp getter(_x = true, _len, num) do
+  defp validate_random(_x = true, _len, num) do
     {:ok, num}
   end
 
-  defp getter(_x = false, len, _num) do
+  defp validate_random(_x = false, len, _num) do
     random_gen(len)
   end
 end
--- test/inner_test.exs
+++ test/inner_test.exs
@@ -4,11 +4,11 @@ defmodule InnerTest do
   alias Luhn.Algo
 
   test "Make sure check function works on number above 10 =" do
-    assert Algo.worker(6) == 3
+    assert Algo.pass_to_check(6) == 3
   end
 
   test "Assure x * 2 less than 10 returns value * 2" do
-    assert Algo.worker(3) == 6
+    assert Algo.pass_to_check(3) == 6
   end
 
   test "random_gen returns valid luhn number" do
